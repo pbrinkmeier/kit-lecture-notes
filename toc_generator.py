0c33093e43bab8c25fcd6d3f521946d05a44a547
Luke Brocke <luke@luk3b.net>
2017-10-20 11:40:59 +0200

    Add list of blacklisted titles to ToC generator

--- toc_generator.py
+++ toc_generator.py
@@ -5,6 +5,13 @@
 import sys
 import re
 
+# blacklisted headers that will not end up in the toc
+blacklist = [
+    "table-of-contents",
+    "contents",
+    "overview"
+]
+
 # returns num of hash symbols (= level of header)
 def count_hash(line):
     cnt = 0
@@ -18,7 +25,7 @@ def convert_link(header):
     # lower case, white spaces are converted to dashes and no characters other
     # than letters and numbers allowed
     link = header.lower().replace(" ", "-")
-    return "#" + re.sub("[^a-zA-Z0-9\-]", "", link)
+    return re.sub("[^a-zA-Z0-9\-]", "", link)
 
 # check line for header symbols ('#')
 def check_line(line):
@@ -32,9 +39,13 @@ def check_line(line):
         return
 
     title = line[(num + 1):]
+    link = convert_link(title)
+
+    if link in blacklist:
+        return
 
     print("    " * (num - 2) + "- ", end='')
-    print("[" + title + "](" + convert_link(title) + ")")
+    print("[" + title + "](#" + link + ")")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
